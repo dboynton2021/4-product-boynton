d497b740837c5692acfc62258973c0af63ea5873
dboynton2021 <39123945+dboynton2021@users.noreply.github.com>
2018-05-30 09:08:51 -0400

    Initial Launch

--- js/topNav.js
+++ js/topNav.js
@@ -1,8 +1,11 @@
-function topNav() {
-    var x = document.getElementById("mobileNav");
+function myFunction() {
+    var x = document.getElementById("navmobile");
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
     } else { 
         x.className = x.className.replace(" w3-show", "");
+         
+       
     }
+ 
 }
